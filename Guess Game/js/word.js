fb8aab57ca99c1f0beb6c455b5d592f70094fd06
frontendtester <frontendtester@mail.com>
2022-08-22 12:10:54 +0500

    My first Game

---
+++ Guess Game/js/word.js
@@ -0,0 +1,155 @@
+const wordList = [
+  {
+      word: "JavaScript",
+      hint: "Dasturlash tili"
+  },
+  {
+      word: "Gitara",
+      hint: "Musiqa Instrumenti"
+  },
+  {
+      word: "Maqsad",
+      hint: "Maqsad yoki Niyat"
+  },
+  
+  {
+      word: "Oltin",
+      hint: "Sariq rangli qimmatbaho metal"
+  },
+  {
+      word: "Aliexpress",
+      hint: "Onlayn xarid qilish sayti"
+  },
+  
+  {
+      word: "Kodlash",
+      hint: "Dasturlash bilan bog'liq"
+  },
+  {
+      word: "Qasoskorlar",
+      hint: "Fantastik film"
+  },
+  
+  {
+      word: "Choqintirgan Ota",
+      hint: "Amerikani kuchli kinosi"
+  },
+  {
+      word: "Gif",
+      hint: "Tasvir uchun fayl formati"
+  },
+  {
+      word: "Ruhiy",
+      hint: "Aql bilan bog'liq"
+  },
+  
+  {
+      word: "Islandiya",
+      hint: "Suv bilan o'ralgan er"
+  },
+  {
+      word: "Hokey",
+      hint: "Mashhur ochiq o'yin"
+  },
+  {
+      word: "Shaxmat",
+      hint: "Yopiq o'yin bilan bog'liq"
+  },
+  {
+      word: "viber",
+      hint: "Ijtimoiy media ilovasi"
+  },
+  {
+      word: "Github",
+      hint: "Kod hosting platformasi"
+  },
+  {
+      word: "Png",
+      hint: "Rasmla fayl formati"
+  },
+  {
+      word: "Kumush",
+      hint: "Qimmatbaho kulrang-oq metall"
+  },
+  {
+      word: "Mobile",
+      hint: "Elektron qurilma"
+  },
+  
+  {
+      word: "Google",
+      hint: "mMashhur qidiruv tizimi"
+  },
+  {
+      word: "Vinetsiya",
+      hint: "Mashhur suv shahri"
+  },
+  {
+      word: "Excel",
+      hint: "Windows uchun microsoft mahsuloti"
+  },
+  {
+      word: "Mysql",
+      hint: "Relyatsion ma'lumotlar bazasi tizimi"
+  },
+ 
+  {
+      word: "Crypto",
+      hint: "Kriptovalyuta bilan bog'liq"
+  },
+
+  {
+      word: "Mars",
+      hint: "Bizning quyosh sistemamizning sayyorasi"
+  },
+  {
+      word: "Proxy",
+      hint: "server ilovasi bilan bog'liq"
+  },
+  {
+      word: "Email",
+      hint: "Xabar almashish bilan bog'liq"
+  },
+  {
+      word: "Html",
+      hint: "Veb uchun belgilash tili"
+  },
+  {
+      word: "Air",
+      hint: "gaz bilan bog'liq"
+  },
+  {
+      word: "Fikr",
+      hint: "Fikr yoki taklif"
+  },
+  {
+      word: "Server",
+      hint: "Kompyuter yoki tizim bilan bog'liq"
+  },
+  {
+      word: "Svg",
+      hint: "Vektor tasvir formati"
+  },
+  
+  {
+      word: "Search",
+      hint: "Biror narsani topish"
+  },
+
+  {
+      word: "Egypt",
+      hint: "Mamlakat nomi"
+  },
+  {
+      word: "joker",
+      hint: "Psixologik triller film"
+  },
+  {
+      word: "Yaponiya",
+      hint: "Rivojlangan davlat nomi"
+  },
+  {
+      word: "photo",
+      hint: "Shaxs yoki sahnaning tasviri"
+  },
+]
